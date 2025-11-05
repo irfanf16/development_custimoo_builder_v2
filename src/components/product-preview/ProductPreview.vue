d71f7a645d6779f539c1b36503b4f4130221c722
Rafael Aguilar <rafa@custimoo.com>
2025-11-05 10:31:40 +0100

    refactor(ProductPreview): comment out unused imports related to 3D assets. For now we are testing with only 2D assets
    - Commented out unused imports from Pinia and ThreePreview to clean up the code.
    - Simplified the component rendering logic to only use CanvasPreview, improving clarity and maintainability.

--- src/components/product-preview/ProductPreview.vue
+++ src/components/product-preview/ProductPreview.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
   import { ref } from 'vue'
-  import { storeToRefs } from 'pinia'
+  // import { storeToRefs } from 'pinia'
   import CanvasPreview from '@/components/customizer-canvas-preview/CanvasPreview.vue'
-  import ThreePreview from './ThreePreview.vue'
-  import { useProductsStore } from '@/stores/products/products.store'
+  // import ThreePreview from './ThreePreview.vue'
+  // import { useProductsStore } from '@/stores/products/products.store'
   import { useElementSize } from '@vueuse/core'
 
-  const products = useProductsStore()
-  const { activeRenderMode: mode } = storeToRefs(products)
+  // const products = useProductsStore()
+  //const { activeRenderMode: mode } = storeToRefs(products)
   const previewContainer = ref<HTMLElement>()
   const elementSize = useElementSize(previewContainer)
 </script>
@@ -15,11 +15,17 @@
 <template>
   <div id="3d-or-2d-preview" ref="previewContainer" class="w-full h-full max-w-full">
     <component
-      :is="mode === '3d' ? ThreePreview : CanvasPreview"
+      :is="CanvasPreview"
       v-if="elementSize.width.value > 0 && elementSize.height.value > 0"
       :width="elementSize.width.value"
       :height="elementSize.height.value"
     />
+    <!-- <component
+      :is="mode === '3d' ? ThreePreview : CanvasPreview"
+      v-if="elementSize.width.value > 0 && elementSize.height.value > 0"
+      :width="elementSize.width.value"
+      :height="elementSize.height.value"
+    /> -->
   </div>
 
   <!-- If needed, we can add a light fade cross-fade here later -->
